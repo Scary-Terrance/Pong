5de4871436e1d6167b5423ac5ff7ab319f0d2fb4
Ben <oeslian@gmail.com>
2020-04-23 11:56:17 -0400

    Added score keeping and ball reset

--- AI.gd
+++ AI.gd
@@ -2,16 +2,14 @@ extends KinematicBody2D
 
 const SPEED = 200
 
-onready var ball = get_parent().get_node("Ball")
-
 var velocity = Vector2(0.0, 0.0)
 
 func _physics_process(delta):
-	var y = 0.0
-	if ball:
+	for ball in get_tree().get_nodes_in_group("Balls"):
+		var y = 0.0
 		if self.position.y < ball.position.y && self.position.y + 64 < 600:
 			y = 1.0
 		elif self.position.y > ball.position.y && self.position.y - 64 > 0:
 			y = -1.0
-	velocity.y = y * SPEED * delta
-	move_and_collide(velocity)
+		velocity.y = y * SPEED * delta
+		move_and_collide(velocity)
--- Game.gd
+++ Game.gd
@@ -1,9 +1,30 @@
 extends Node2D
 
-onready var ball = $Ball
+onready var ball_sack = preload("res://Ball.tscn")
+onready var spawn = $Spawn
 
-func _physics_process(delta):
-	if ball.position.x - 16 <= 0:
-		get_tree().reload_current_scene()
-	elif ball.position.x + 16 >= 1024:
-		get_tree().reload_current_scene()
+var ball
+var player_score = 0
+var ai_score = 0
+
+func _spawn_ball():
+	ball = ball_sack.instance()
+	ball.set_name("Ball")
+	ball.position = spawn.position
+	add_child(ball)
+
+func _ready():
+	_spawn_ball()
+
+func _physics_process(_delta):
+	if ball:
+		if ball.position.x - 16 <= 0:
+			ai_score += 1
+			self.remove_child(ball)
+			_spawn_ball()
+			print(ai_score)
+		elif ball.position.x + 16 >= 1024:
+			player_score += 1
+			self.remove_child(ball)
+			_spawn_ball()
+			print(player_score)
